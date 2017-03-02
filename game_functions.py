c2f576079129bee810867e2f6e0779a6743d1240
Sarah Basinger <sarrey@gmail.com>
2017-03-01 22:49:21 -0500

    fixed crashing when zombies appeared

--- game_functions.py
+++ game_functions.py
@@ -32,8 +32,8 @@ def update_screen(screen, game_settings, background, zombies, squares, plants, b
 	for zombie in zombies.sprites():
 		zombie.update_me()
 		zombie.draw_me()
-		if zombie.rect.left <= screen.rect.left:
-			game_settings.game_active = False
+		if zombie.rect.left <= zombie.screen_rect.left:
+			game_settings.game_active = False;
 
 	for plant in plants:
 		plant.draw_me()
